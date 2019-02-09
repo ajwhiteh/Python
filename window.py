04ec63105cfdd29581f1cf5c05eb0f44355e1437
ajwhiteh <andrewj.whitehurst@gmail.com>
2019-02-09 16:25:26 -0700

    Add files via upload

---
+++ window.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon
+
+
+
+class App(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.title = 'This Window Title'
+		self.left = 10
+		self.top = 10
+		self.width = 640
+		self.height = 480
+		self.initUI()
+		
+	def initUI(self):
+		self.setWindowTitle(self.title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
+		
+		button = QPushButton('Py button', self)
+		button.setToolTip('This is a button')
+		button.move(100,70)
+		button.clicked.connect(self.on_click)
+		
+		self.show()
+		
+	@pyqtSlot()
+	def on_click(self):
+		print('Button click')
+		
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
